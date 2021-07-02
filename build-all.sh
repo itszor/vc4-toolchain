2f57f093ef6bc77470152f00e98f38843e8fbbc3
Julian Brown <julianbrown99@gmail.com>
2021-07-02 10:00:57 +0100

    Default to not regenenerating CGEN output files.
    This is a workaround for issue 16, 17, etc.

--- build-all.sh
+++ build-all.sh
@@ -7,8 +7,8 @@ HERE="$(cd "$SCRIPTDIR" && pwd)"
 PARALLEL="-j 2"
 
 # Remove this if you don't want to mess with CGEN.
-WITHCGEN="--enable-cgen-maint"
-# WITHCGEN=
+# WITHCGEN="--enable-cgen-maint"
+WITHCGEN=
 
 # Set this to false to disable C++ (speed up build a bit).
 WITHCXX=true
